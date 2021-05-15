b2103b66289614fe1e1939ba2e12d374ed8677cc
Matthew Potts <72045364+andromeda-voyager@users.noreply.github.com>
2021-05-15 09:29:35 -0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,8 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 import { Router } from "./router.ts"
-import { listen } from "./chat.ts"
-
-listen();
 
 const router = new Router();
 const s = serve({ port: 8000 });
@@ -10,7 +7,7 @@ const s = serve({ port: 8000 });
 for await (const req of s) {
   //req.respond({ body: "Hello World\n" });
 
-  router.get("/create-encrypted-channel", req, (query:any)=> {
+  router.get("/test", req, (query:any)=> {
     var q = req.url
     console.log(query.name);
     req.respond(req);
